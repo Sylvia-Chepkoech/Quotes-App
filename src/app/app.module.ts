6f298dda061c20b01182978294586f231bcd524c
Sylvia-Chepkoech <slycheche@gmail.com>
2022-04-11 00:48:53 +0300

    generate quotes component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { HomeComponent } from './home/home.component';
+import { QuotesComponent } from './quotes/quotes.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    HomeComponent
+    HomeComponent,
+    QuotesComponent
   ],
   imports: [
     BrowserModule,
